e15c23f56f6bd0bf534b1e424e52d606cd7c4e88
vevdokimenko <evdokimenko@hotmail.com>
2021-10-29 23:35:44 +0300

    homework - extra

--- src/extra/MyClass.java
+++ src/extra/MyClass.java
@@ -5,14 +5,19 @@ import java.util.List;
 
 public class MyClass<T> {
     public static <T> List<T> factoryMethod(T element) {
-        List<T> list = new ArrayList<T>();
+        List<T> list = new ArrayList<>();
         list.add(element);
         return list;
     }
 
     public static void main(String[] args) {
+        // 4 : Integer
         System.out.println(factoryMethod(4).get(0) + " : " + factoryMethod(4).get(0).getClass().getSimpleName());
+
+        // String : String
         System.out.println(factoryMethod("String").get(0) + " : " + factoryMethod("String").get(0).getClass().getSimpleName());
+
+        // true : Boolean
         System.out.println(factoryMethod(true).get(0) + " : " + factoryMethod(true).get(0).getClass().getSimpleName());
     }
 }
